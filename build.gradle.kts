7db77e542ea036916a5d2bd22275e090d6773099
jandeerl <johnderlukiewicz@gmail.com>
2024-08-13 09:40:25 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,22 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation(platform("org.junit:junit-bom:5.9.1"))
+    implementation("org.junit.jupiter:junit-jupiter")
+    implementation("io.appium:java-client:9.2.2")
+    implementation("com.fasterxml.jackson.core:jackson-core:2.17.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.17.1")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/test/java/DriverProvider.java
@@ -0,0 +1,26 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
+
+public class DriverProvider {
+
+    public static UiAutomator2Options loadCapabilities(String filePath) throws
+            IOException {
+        ObjectMapper objectMapper = new ObjectMapper();
+        InputStream is = new FileInputStream(filePath);
+        return objectMapper.readValue(is, UiAutomator2Options.class);
+    }
+
+    static AndroidDriver provideDriver() throws IOException {
+        return new AndroidDriver(
+                new URL("http://127.0.0.1:4723"),
+                loadCapabilities("src/test/resources/android_capabilities.json")
+        );
+    }
+}
